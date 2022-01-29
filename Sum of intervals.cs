723669150e6bfe5117a3e20cd465ed0c51198847
MariiaDeshshynska <86978280+MariiaDeshshynska@users.noreply.github.com>
2022-01-30 00:11:59 +0200

    Add Sum of intervals

---
+++ Sum of intervals.cs
@@ -0,0 +1,77 @@
+/*
+Sum of intervals
+Write a function called sumIntervals/sum_intervals() that accepts an array of intervals, and returns the sum of all the interval lengths. Overlapping intervals should only be counted once.
+
+Intervals
+Intervals are represented by a pair of integers in the form of an array. The first value of the interval will always be less than the second value. Interval example: [1, 5] is an interval from 1 to 5. The length of this interval is 4.
+
+Overlapping Intervals
+List containing overlapping intervals:
+
+[
+   [1,4],
+   [7, 10],
+   [3, 5]
+]
+The sum of the lengths of these intervals is 7. Since [1, 4] and [3, 5] overlap, we can treat the interval as [1, 5], which has a length of 4.
+
+Examples:
+sumIntervals( [
+   [1,2],
+   [6, 10],
+   [11, 15]
+] ); // => 9
+
+sumIntervals( [
+   [1,4],
+   [7, 10],
+   [3, 5]
+] ); // => 7
+
+sumIntervals( [
+   [1,5],
+   [10, 20],
+   [1, 6],
+   [16, 19],
+   [5, 11]
+] ); // => 19
+*/
+        public static int SumIntervals((int, int)[] intervals)
+        {
+            if (intervals.Length != 0)
+            {
+
+                Array.Sort(intervals);
+                int min = intervals[0].Item1;
+                int max = intervals[0].Item2;
+                int sum = 0;
+                foreach (var item in intervals)
+                {
+                    if ((item.Item1 < max && item.Item1 >= min))
+                    {
+                        if (item.Item2 > max)
+                        {
+                            sum += item.Item2 - max;
+                            max = item.Item2;
+                        }
+                        else if(item.Item2 == max && item.Item1==min)
+                        {
+                            sum += (item.Item2 - item.Item1);
+                        }
+
+                    }
+                    else
+                    {
+                            sum += (item.Item2 - item.Item1);
+                            min = item.Item1;
+                            max = item.Item2;
+                    }
+                }
+                foreach (var item1 in intervals)
+                {
+                    Console.WriteLine(item1);
+                }
+                return sum;
+            }
+            else return 0;
+        }
